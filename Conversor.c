d4743e008c3a7df71bad26104356e80cfc90f018
Christian Schmidt <christian.schmidt@acad.ufsm.br>
2025-05-18 19:36:13 -0300

    Update Conversor.c

--- Conversor.c
+++ Conversor.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 void calc_idade(){
-    int idade, meses, dias;
-    float semanas, horas;
+    int idade, meses;
+    float semanas, horas, dias;
     
     printf("Digite sua idade\n");
     scanf("%d", &idade);
     
     meses = idade * 12;
-    dias = idade * 365;
+    dias = idade * 365.25;
     semanas = dias / 7;
     horas = dias * 24;
     
-    printf("voce tem %d meses, %.2f semanas e %.2d dias, %.2f horas", meses, semanas, dias, horas);
+    printf("voce tem %d meses, %.2f semanas e %.2f dias, %.2f horas", meses, semanas, dias, horas);
 }
 int main()
 {
